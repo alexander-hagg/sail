af3f6c61b4dcbfe2e3406c21525723e4302d2b20
Alexander Hagg <info@haggdesign.de>
2017-12-12 11:17:19 +0100

    Domains: mirror, curvature feature: added two curvature determination lines

--- domains/mirror/feature/curvature.m
+++ domains/mirror/feature/curvature.m
@@ -1,69 +1,51 @@
+[FV, ~, ffdP] = mirror_ffd_Express(0.5 + zeros(1,41), 'mirrorBase.stl');
+
 % Visualize
-figure(1);
-plot3(FV{shapeID}.vertices(1,:),FV{shapeID}.vertices(2,:),FV{shapeID}.vertices(3,:),'x');
-view(107,90);
-
-
-figure(2);hold on;
+figure(2);hold off;
 plot3(back(:,1), back(:,2),back(:,3),'x');
-view(115,0);
+view(112,90);
 %
 hold on;
-scatter3(line1(:,1),line1(:,2),line1(:,3),'filled');
-scatter3(line2(:,1),line2(:,2),line2(:,3),'filled');
-scatter3(line3(:,1),line3(:,2),line3(:,3),'filled');
-scatter3(line4(:,1),line4(:,2),line4(:,3),'filled');
-scatter3(line5(:,1),line5(:,2),line5(:,3),'filled');
+for i=1:length(lines)
+scatter3(lines{i}(:,1),lines{i}(:,2),lines{i}(:,3),'filled');
+text(lines{i}(1,1),lines{i}(1,2),lines{i}(1,3),['line' int2str(i)]);
+plot3(lines{i}([1 end],1),lines{i}([1 end],2),lines{i}([1 end],3));
+end
 %
 
 xlabel('x');ylabel('y');zlabel('z');
 %view(-270,0);
 grid on;
-axis equal;
 
 %% Find IDs
-[~,index_InMesh] = intersect(FV{shapeID}.vertices',back,'rows')
+[~,index_InMesh] = intersect(FV.vertices',back,'rows')
+for i=1:length(lines)
+    [~,indices] = intersect(back,lines{i},'rows')
+    linIndices{i} = index_InMesh(indices)'
+end
 
-
-[~,indexLine] = intersect(back,line1,'rows')
-linIndices{1} = index_InMesh(indexLine)'
-[~,ii] = intersect(back,line2,'rows')
-linIndices{2} = index_InMesh(ii)'
-indexLine = [indexLine;ii];
-[~,ii] = intersect(back,line3,'rows')
-linIndices{3} = index_InMesh(ii)'
-indexLine = [indexLine;ii];
-[~,ii] = intersect(back,line4,'rows')
-linIndices{4} = index_InMesh(ii)'
-indexLine = [indexLine;ii];
-[~,ii] = intersect(back,line5,'rows')
-linIndices{5} = index_InMesh(ii)'
-indexLine = [indexLine;ii];
-
+indexLine = cell2mat(linIndices)
 %% Visualize random shapes and curvature
 if ~exist('curvs','var')
     for ii=1:1000
-        [FV{shapeID}, ~, ffdP{shapeID}] = ...
+        [FV, ~, ffdP] = ...
             mirror_ffd_Express(0.5 + 0.2*randn(1,41), 'mirrorBase.stl');
-        %[FV{shapeID}, ~, ffdP{shapeID}] = ...
-        %    mirror_ffd_Express(0.5 - 0.2*ones(1,41), 'mirrorBase.stl');
-        
         % figure(3);
         % hold off;
-        % plot3(FV{shapeID}.vertices(1,:),FV{shapeID}.vertices(2,:),FV{shapeID}.vertices(3,:),'x');
+        % plot3(FV.vertices(1,:),FV.vertices(2,:),FV.vertices(3,:),'x');
         % hold on;
         % for i=1:5
-        % scatter3(FV{shapeID}.vertices(1,linIndices{i}),FV{shapeID}.vertices(2,linIndices{i}),FV{shapeID}.vertices(3,linIndices{i}),'filled');
+        % scatter3(FV.vertices(1,linIndices{i}),FV.vertices(2,linIndices{i}),FV.vertices(3,linIndices{i}),'filled');
         % end
         % %
         for i=1:5
-            line = FV{shapeID}.vertices(:,linIndices{i});
+            line = FV.vertices(:,linIndices{i});
             % Ratio between line length and length between start and end point
             startToEnd(i) = pdist2(line(:,1)',line(:,end)');
             curv(i) = sum(diag(squareform(pdist(line')),1))/startToEnd(i);
         end
         for i=1:5
-            line = FV{shapeID}.vertices(:,linIndices{i});
+            line = FV.vertices(:,linIndices{i});
             curv(i) = curv(i)/max(startToEnd);
             %     text(line(1,1),line(2,1),line(3,1),num2str(curv(i)),'FontSize',14)
         end
@@ -78,32 +60,30 @@ histogram(curvs,'Normalization','pdf');xlabel('Total Curvature');ylabel('pdf');t
 
 %% Get some random shapes and show curvature
 fig(1) = figure(5);
-for ii=1:8
-    subplot(4,2,ii);
+for ii=1:9
+    subplot(3,3,ii);
     hold off;
     [FV{ii}, ~, ffdP{ii}] = ...
-        mirror_ffd_Express(0.5 + ii*0.03*randn(1,41), 'mirrorBase.stl');
+        mirror_ffd_Express(0.5 + ii*0.05*randn(1,41), 'mirrorBase.stl');
     scatter3(FV{ii}.vertices(1,:), FV{ii}.vertices(2,:),FV{ii}.vertices(3,:),4,'filled');
     view(-30,10);
     hold on;
-
-    for i=1:5
+    tic
+    for i=1:length(linIndices)
         line = FV{ii}.vertices(:,linIndices{i});
         % Ratio between line length and length between start and end point
         startToEnd(i) = pdist2(line(:,1)',line(:,end)');
         curv(i) = sum(diag(squareform(pdist(line')),1))/startToEnd(i);
         scatter3(line(1,:),line(2,:),line(3,:),'filled');
     end
-    for i=1:5
-        line = FV{ii}.vertices(:,linIndices{i});
+    toc
+    for i=1:length(linIndices)
         curv(i) = curv(i)/max(startToEnd);
-        %     text(line(1,1),line(2,1),line(3,1),num2str(curv(i)),'FontSize',14)
     end
-    
     title(['' num2str(sum(curv))]);
     disp(['Total Curvature Approximation: ' num2str(sum(curv))]);
     drawnow;
     
 end
 
-save_figures(fig, './', ['fff_'], 12, [10 6]);
+%save_figures(fig, './', ['fff_'], 12, [10 6]);
